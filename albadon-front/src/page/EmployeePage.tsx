a2f19661c3625fdf95b0a576f97888f13a4de967
ga0hyeon <idoll0513@gmail.com>
2021-07-11 14:47:17 +0900

    modify column def

--- albadon-front/src/page/EmployeePage.tsx
+++ albadon-front/src/page/EmployeePage.tsx
@@ -3,6 +3,8 @@ import React, { useEffect, useState } from 'react'
 import { getEmployeeListByStoreId } from '../service/EmployeeService';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
+import { ValueFormatterParams } from 'ag-grid-community';
+import moment from 'moment';
 
 
 export const EmployeePage : React.FC = ()=>{
@@ -10,7 +12,7 @@ export const EmployeePage : React.FC = ()=>{
 
     useEffect(()=>{
        
-            getEmployeeListByStoreId("1").then(res => setEmployeeList(res.data.data))
+            getEmployeeListByStoreId("1").then(res => setEmployeeList(res.data))
             
     }, [])
 
@@ -18,15 +20,22 @@ export const EmployeePage : React.FC = ()=>{
 
 const columnInfo = [
 
-    { key : "employeeName", label: "이름", sortable: true, filter: false, width: 90},
-    { key : "employeePhoneNumber", label: "전화번호 ", sortable: true, filter: false, width: 160},
-    { key : "holidayWage", label: "주간시급", sortable: true, filter: false, width: 100},
-    { key : "nightWage", label: "야간시급 ", sortable: true, filter: false, width: 100},
-    { key : "startDate", label: "근무시작일", sortable: true, filter: false, width: 140},
-    { key : "endDate", label: "근무종료일 ", sortable: true, filter: false, width: 140},
-    { key : "role", label: "역할 ", sortable: true, filter: false,  width: 110},
+    { key : "employeeName", label: "이름", sortable: true, filter: false, width: 90, type: "string"},
+    { key : "employeePhoneNumber", label: "전화번호 ", sortable: true, filter: false, width: 160, type: "string"},
+    { key : "holidayWage", label: "주간시급", sortable: true, filter: false, width: 100, type: "number"},
+    { key : "nightWage", label: "야간시급 ", sortable: true, filter: false, width: 100, type: "number"},
+    { key : "startDate", label: "근무시작일", sortable: true, filter: false, width: 140, type: "date"},
+    { key : "endDate", label: "근무종료일 ", sortable: true, filter: false, width: 140, type: "date"},
+    { key : "role", label: "역할 ", sortable: true, filter: false,  width: 110, type: "string"},
 ]
-    
+    const valueFormatter = (params: ValueFormatterParams)=>{
+        if(params.colDef.type === 'date'){
+            return moment(params.value).format('YYYY/MM/DD')
+        }else{
+            return params.value
+        }
+        
+    }
     return <div id="EmployeePage">
        
 
@@ -34,7 +43,7 @@ const columnInfo = [
            <AgGridReact
                rowData={employeeList}>
                 {columnInfo.map(column=>{
-                    return  <AgGridColumn field={column.key} headerName={column.label} sortable={column.sortable} width={column.width} filter={column.filter}></AgGridColumn>
+                    return  <AgGridColumn field={column.key} headerName={column.label} sortable={column.sortable} width={column.width} filter={column.filter} valueFormatter={valueFormatter} type={column.type}></AgGridColumn>
                 })}
            </AgGridReact>
        </div></div>
